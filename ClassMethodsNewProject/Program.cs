ee3ab8422c238e93795b2744e9af00f2f48f6e6f
GitStudent2022 <torelgame@mail.ru>
2022-09-17 16:12:53 +0300

    Добавьте файлы проекта.

---
+++ ClassMethodsNewProject/Program.cs
@@ -0,0 +1,67 @@
+﻿using Cout = System.Console;
+
+namespace ClassMethodsNewProject
+{
+    class Math
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+
+    class User
+    {
+        //public const string StrConst = "Constant string";
+
+        //public readonly string StrReadOnly;
+        public string Name { get; set; }
+
+        public User(string str = "Anonim")
+        {
+            Name = str;
+        }
+
+        public void PrintConsole()
+        {
+            Cout.WriteLine(Name);
+        }
+
+        public void Method(object? obj)
+        {
+            if(obj != null)
+                Console.WriteLine(obj.ToString());
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math math = new Math();
+
+            //Console.WriteLine(Math.Sum(10, 20));
+            //Console.WriteLine(math.Sum(10, 20, 30));
+
+            //User user = new User();
+
+            User user = new User();
+
+            if(user != null)
+                user.PrintConsole();
+
+            user?.PrintConsole();
+
+            int? x = 10;
+            user?.Method(x);
+
+            int y = 20 + x.Value;
+            int z = x ?? 0;
+        }
+    }
+}
